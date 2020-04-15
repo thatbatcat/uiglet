ea37753e0ea00422bb70bbbe1e7966aa60c55c6d
Ryan Bailey <ryancbailey3500@gmail.com>
2020-04-16 00:57:39 +0100

    Removed dependency on non-existant colors file

--- graphics/misc.py
+++ graphics/misc.py
@@ -1,10 +1,7 @@
 from pyglet.gl import *
 
-from ...colors import backgroundColor
-
 #author Ryan Bailey
 
-def clear():
-    color = backgroundColor()
+def clear(color): #color is an rgba tuple with each value going from 0 --> 255 
     glClearColor(color[0]/255, color[1]/255, color[2]/255, color[3]/255)
     glClear(GL_COLOR_BUFFER_BIT)
