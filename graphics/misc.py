8e1c840db5bba8643b5ba46d57f17235ea4dad70
Ryan Bailey <ryancbailey3500@gmail.com>
2020-04-16 01:26:39 +0100

    color is a list, not a tuple

--- graphics/misc.py
+++ graphics/misc.py
@@ -2,6 +2,6 @@ from pyglet.gl import *
 
 #author Ryan Bailey
 
-def clear(color): #color is an rgba tuple with each value going from 0 --> 255 
+def clear(color): #color should be an [r,g,b,a] list where every value is 8-bit
     glClearColor(color[0]/255, color[1]/255, color[2]/255, color[3]/255)
     glClear(GL_COLOR_BUFFER_BIT)
